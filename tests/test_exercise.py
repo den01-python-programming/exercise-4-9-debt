ff20b7c9f61a29fd2ffeb894d7871c1724c25e21
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 15:54:50 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,6 +1,20 @@
 import pytest
-import src.exercise
+from src.debt import Debt
 
-def test_exercise():
-    #implement tests here
-    assert 0 == 0
+def test_exercise(capsys):
+    mortgage = Debt(120000.0, 1.01)
+    mortgage.print_balance()
+
+    mortgage.wait_one_year()
+    mortgage.print_balance()
+
+    years = 0
+
+    while (years < 20):
+        mortgage.wait_one_year()
+        years = years + 1
+
+    mortgage.print_balance()
+
+    out, err = capsys.readouterr()
+    assert out == "120000.0\n121200.0\n147887.0328416936\n"
